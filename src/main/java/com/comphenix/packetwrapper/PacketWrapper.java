f66e9df14274d412b8c604887babb151389de428
lukas81298 <lukas81298@gommehd.net>
2023-05-12 18:16:29 +0200

    fixed version check

--- src/main/java/com/comphenix/packetwrapper/PacketWrapper.java
+++ src/main/java/com/comphenix/packetwrapper/PacketWrapper.java
@@ -3,10 +3,12 @@ package com.comphenix.packetwrapper;
 import com.comphenix.protocol.utility.MinecraftVersion;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public class PacketWrapper extends JavaPlugin {
     @Override
     public void onEnable() {
-        if(MinecraftVersion.getCurrentVersion() != MinecraftVersion.FEATURE_PREVIEW_2) {
+        if(!Objects.equals(MinecraftVersion.getCurrentVersion(), MinecraftVersion.FEATURE_PREVIEW_2)) {
             this.getLogger().severe("Incompatible version of Minecraft. This version of PacketWrapper is only compatible with Minecraft 1.19.4 but you are using " + MinecraftVersion.getCurrentVersion());
         }
     }
